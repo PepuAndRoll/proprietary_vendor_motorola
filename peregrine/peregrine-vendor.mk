a4aff45bd96e7d821eb3a758ab917de4efb4cc80
Pepu <pepuandroll@gmail.com>
2014-12-10 10:46:59 +0100

    Update peregrine-vendor.mk

--- peregrine/peregrine-vendor.mk
+++ peregrine/peregrine-vendor.mk
@@ -16,4 +16,4 @@
 
 # Pick up overlay for features that depend on non-open-source files
 
-$(call inherit-product, vendor/motorola/peregrine/peregrine-vendor-blobs.mk)
+$(call inherit-product, vendor/motorola/titan/titan-vendor-blobs.mk)
